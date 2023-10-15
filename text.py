bdca883b1866647eb2e4ec1e8a52c0aafca04079
yashwanthp11 <112404605+yashwanthp11@users.noreply.github.com>
2023-10-15 11:35:31 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r') as file:
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, file.read())
+        root.title(f"Text Editor - {file_path}")
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))
+        root.title(f"Text Editor - {file_path}")
+
+root = tk.Tk()
+root.title("Text Editor")
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+text = tk.Text(root)
+text.pack(expand=tk.YES, fill=tk.BOTH)
+
+root.mainloop()
